f6fb700e417922cd1059fb922398fb106046ed39
Riccardo Florio <riccardo.florio20@gmail.com>
2023-10-30 10:02:09 +0100

    Completamento traduzione Industrial Robotics Exercise e aggiornamento Report.pdf

---
+++ Industrial robotics/+Functions/Anthropomorphic_Inv_Kin.m
@@ -0,0 +1,29 @@
+function [Q]=Anthropomorphic_Inv_Kin(L,P)
+% Anthropomorphic from Siciliano's book
+Px=P(1);
+Py=P(2);
+Pz=P(3);
+%
+% workspace test
+%
+d=norm(P);
+if d<=(L(2)+L(3)) 
+q1=atan2(Py,Px);
+c1=cos(q1);
+s1=sin(q1);
+%
+c3=((Px)^2+Py^2+(Pz)^2-L(2)^2-L(3)^2)/(2*L(2)*L(3));
+s3=sqrt(1-c3^2);
+q3=atan2(s3,c3);
+%compute Q(2)
+A=[L(2)+L(3)*c3 L(3)*s3; -L(3)*s3 L(2)+L(3)*c3];
+B=inv(A)*[Pz; sqrt(Px^2+Py^2)];
+q2=atan2(B(1),B(2));
+
+Q=[q1;q2;q3];
+else
+    Q=[0;0;0]
+end
+    
+
+end
